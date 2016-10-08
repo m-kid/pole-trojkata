0f1131fa0252ee96215385b60e9de02e151f5dba
m-kid <maciej.kidawa2@gmail.com>
2016-10-09 00:56:46 +0200

    conting utriangle area

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = prompt('Podaj dowolną liczbę'),
+    h = a/3,
+    triangleArea = a*h/2;
 
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea)
